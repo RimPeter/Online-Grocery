a2edf2f8cededd2b61529cb40d1179a63f11d788
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-08-21 09:46:22 +0000

    Create index.html; add databases for elephantSQL

--- grocery/settings.py
+++ grocery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'grocery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online-grocery',
+        'USER': 'bbgkvqwy',
+        'PASSWORD': 'ML13tG1JfeOWgYwRyCGaCIIHJMBnKKIT',
+        'HOST': 'postgres://bbgkvqwy:ML13tG1JfeOWgYwRyCGaCIIHJMBnKKIT@mel.db.elephantsql.com/bbgkvqwy',
+        'PORT': 'your_port',
     }
 }
 
