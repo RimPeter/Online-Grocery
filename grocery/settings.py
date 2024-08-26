ab35d9c3dddf9b2c9cbb0c27e456121c9a5902fa
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-08-26 07:47:59 +0000

    Full reset for all migrations

--- grocery/settings.py
+++ grocery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'home',
     
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -84,7 +85,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -110,7 +111,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'grocery.wsgi.application'
 
